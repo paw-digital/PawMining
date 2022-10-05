fced09cefa27c80aa98da228b08bf5ac05a6e878
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2022-10-05 23:45:57 +0200

    Update mine.sh

--- linux/mine.sh
+++ linux/mine.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Your PAW address : " paw
 chmod +x bin/xmrig
-bash bin/xmrig --opencl --cuda -o mining.paw.digital:3333 -u $paw -k --coin monero -a rx/0
+bin/./xmrig --opencl --cuda -o mining.paw.digital:3333 -u $paw -k --coin monero -a rx/0
